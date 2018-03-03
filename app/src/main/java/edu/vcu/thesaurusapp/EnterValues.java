777a53c82c353fbec2da2a24e12217b6a0acfe07
Austin Underwood <agugold3@gmail.com>
2018-03-03 12:04:17 -0500

    Added project

---
+++ app/src/main/java/edu/vcu/thesaurusapp/EnterValues.java
@@ -0,0 +1,16 @@
+package edu.vcu.thesaurusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by agugo on 3/3/2018.
+ */
+
+public class EnterValues extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_values);
+    }
+}
---
+++ app/src/main/java/edu/vcu/thesaurusapp/WelcomeScreen.java
@@ -0,0 +1,22 @@
+package edu.vcu.thesaurusapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+
+    public void onButtonClick(View v) {
+        if(v.getId() == R.id.Bentervalues) {
+            Intent i = new Intent(WelcomeScreen.this, EnterValues.class);
+            startActivity(i);
+        }
+    }
+}
